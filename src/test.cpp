e708b842b509db5ea3271c49e94e4768624b16a8
LieBtrau <prog.send@gmail.com>
2015-02-07 16:54:56 +0100

    Code is more or less operational.

--- src/test.cpp
+++ src/test.cpp
@@ -1,76 +1,246 @@
 /*
-Arduino template for Qt
-Downloaded from http://www.lucidarme.me
-
-In the menu Tools>Option set in the Terminal field : xterm -e
-
-Set the following parameters in the project tab :
-Build directory : add /src at the end of the path
-Build steps : make all
-Clean steps : make clean
-
-
+Based on code from Spark Fun Electronics, Nathan Seidle
 */
-
 
 // Access to the Arduino Libraries
 #include "test.h"
-
-/*
-  Turns on an LED on for one second, then off for one second, repeatedly.
-*/
+// Define game parameters
+const byte ROUNDS_TO_WIN=13;        //Number of rounds to succesfully remember before you win. 13 is do-able.
+const word ENTRY_TIME_LIMIT=5000;   //Amount of time to press a button before game times out. 3000ms = 3 sec
 
 // Define the pin where the led is connected
-const byte PIN_LED_D1=3;
-const byte PIN_LED_D2=0;
-const byte PIN_LED_D3=1;
-const byte PIN_LED_D4=2;
+const byte LED_GREEN=3;
+const byte LED_RED=0;
+const byte LED_YELLOW=1;
+const byte LED_BLUE=2;
+
 const byte PIN_SPEAKER=5;
-byte iCtr=0;
 TinyDebugSerial mySerial = TinyDebugSerial();
 Music ms(PIN_SPEAKER);
-//returns for bare copper: 72 untouched to 860 touched
-//returns for copper covered with plastic tape: 20 untouched to 320 touched
 //Touch limit values are average of untouched and touched conditions.  These are determined by testing each pad.
-QtouchAdc qtTP1(ADC_Input_ADC0, ADC_Input_ADC1, 200);
-QtouchAdc qtTP2(ADC_Input_ADC1, ADC_Input_ADC2, 200);
-QtouchAdc qtTP3(ADC_Input_ADC2, ADC_Input_ADC3, 200);
-QtouchAdc qtTP4(ADC_Input_ADC3, ADC_Input_ADC0, 200);
+QtouchAdc qtBLUE(ADC_Input_ADC0, ADC_Input_ADC1, 170);
+QtouchAdc qtRED(ADC_Input_ADC1, ADC_Input_ADC2, 90);
+QtouchAdc qtGREEN(ADC_Input_ADC2, ADC_Input_ADC3, 100);
+QtouchAdc qtYELLOW(ADC_Input_ADC3, ADC_Input_ADC0, 100);
+
+// Game state variables
+byte gameBoard[32]; //Contains the combination of buttons as we advance
+byte gameRound = 0; //Counts the number of succesful rounds the player has made it through
 
 void setup()
 {
     // Initialize Arduino Librairies
     init();
-    pinMode(PIN_LED_D1, OUTPUT);
-    pinMode(PIN_LED_D2, OUTPUT);
-    pinMode(PIN_LED_D3, OUTPUT);
-    pinMode(PIN_LED_D4, OUTPUT);
-    pinMode(PIN_SPEAKER, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
     ms.init();
-    ms.winner_sound();
-    ms.playMusic();
-    delay(3000);
-    ms.playMusic();
-
+//    mySerial.begin(9600);
 }
+
+//void loop()
+//{
+//    int value;
+//    qtRED.isButtonTouched(value);
+//    mySerial.print(value);
+//    mySerial.print(" ");
+//    qtBLUE.isButtonTouched(value);
+//    mySerial.print(value);
+//    mySerial.print(" ");
+//    qtYELLOW.isButtonTouched(value);
+//    mySerial.print(value);
+//    mySerial.print(" ");
+//    qtGREEN.isButtonTouched(value);
+//    mySerial.println(value);
+//    delay(1000);
+//}
+
 
 void loop()
 {
+    attractMode(); // Blink lights while waiting for user to press a button
+
+    // Indicate the start of game play
+    setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE | CHOICE_YELLOW); // Turn all LEDs on
+    delay(1000);
+    setLEDs(CHOICE_OFF); // Turn off LEDs
+    delay(250);
+
+        // Play memory game and handle result
+        if (play_memory() == true)
+            play_winner(); // Player won, play winner tones
+        else
+            play_loser(); // Player lost, play loser tones
+}
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//The following functions are related to game play only
+
+// Play the regular memory game
+// Returns 0 if player loses, or 1 if player wins
+boolean play_memory(void)
+{
+    randomSeed(millis()); // Seed the random generator with random amount of millis()
+    
+    for(gameRound=0; gameRound < ROUNDS_TO_WIN; gameRound++)
+    {
+        gameBoard[gameRound] = 1<<random(0, 4); //min (included), max (exluded) // Add this new button to the game array
+        playMoves(); // Play back the current game board
+        // Then require the player to repeat the sequence.
+        for (byte currentMove = 0 ; currentMove <= gameRound ; currentMove++)
+        {
+            byte choice = wait_for_button(); // See what button the user presses
+            if (choice == 0) return false; // If wait timed out, player loses
+            if (choice != gameBoard[currentMove]) return false; // If the choice is incorect, player loses
+        }
+        delay(1000); // Player was correct, delay before playing moves
+    }
+    return true; // Player made it through all the rounds to win!
+}
+
+// Plays the current contents of the game moves
+void playMoves(void)
+{
+    for (byte currentMove = 0 ; currentMove < gameRound ; currentMove++)
+    {
+        toner(gameBoard[currentMove], 150);
+        // Wait some amount of time between button playback
+        // Shorten this to make game harder
+        delay(150); // 150 works well. 75 gets fast.
+    }
+}
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//The following functions control the hardware
+
+// Lights a given LEDs
+// Pass in a byte that is made up from CHOICE_RED, CHOICE_YELLOW, etc
+void setLEDs(byte leds)
+{
+    digitalWrite(LED_RED, (leds & CHOICE_RED) ? HIGH : LOW);
+    digitalWrite(LED_GREEN, (leds & CHOICE_GREEN) ? HIGH : LOW);
+    digitalWrite(LED_YELLOW, (leds & CHOICE_YELLOW) ? HIGH : LOW);
+    digitalWrite(LED_BLUE, (leds & CHOICE_BLUE) ? HIGH : LOW);
+}
+
+// Wait for a button to be pressed.
+// Returns one of LED colors (LED_RED, etc.) if successful, 0 if timed out
+byte wait_for_button(void)
+{
+    long startTime = millis(); // Remember the time we started the this loop
+    
+    while ( (millis() - startTime) < ENTRY_TIME_LIMIT) // Loop until too much time has passed
+    {
+        byte button = checkButton();
+        if (button != CHOICE_NONE)
+        {
+            toner(button, 150); // Play the button the user just pressed
+            while(checkButton() != CHOICE_NONE) ;  // Now let's wait for user to release button
+            delay(10); // This helps with debouncing and accidental double taps
+            return button;
+        }
+    }
+    return CHOICE_NONE; // If we get here, we've timed out!
+}
+
+// Returns a '1' bit in the position corresponding to CHOICE_RED, CHOICE_GREEN, etc.
+byte checkButton(void)
+{
     int value;
-    digitalWrite(PIN_LED_D3,qtTP1.isButtonTouched(value) ? HIGH : LOW);
-    digitalWrite(PIN_LED_D2,qtTP2.isButtonTouched(value) ? HIGH : LOW);
-    digitalWrite(PIN_LED_D1,qtTP3.isButtonTouched(value) ? HIGH : LOW);
-    digitalWrite(PIN_LED_D4,qtTP4.isButtonTouched(value) ? HIGH : LOW);
-    if(qtTP1.isButtonTouched(value)){
-        ms.playTone(2794, 100);
+    if(qtRED.isButtonTouched(value)){
+        return CHOICE_RED;
     }
-    if(qtTP2.isButtonTouched(value)){
-        ms.playTone(2093, 100);
+    if(qtBLUE.isButtonTouched(value)){
+        return CHOICE_BLUE;
     }
-    if(qtTP3.isButtonTouched(value)){
-        ms.playTone(2349, 100);
+    if(qtYELLOW.isButtonTouched(value)){
+        return CHOICE_YELLOW;
     }
-    if(qtTP4.isButtonTouched(value)){
-        ms.playTone(2637, 100);
+    if(qtGREEN.isButtonTouched(value)){
+        return CHOICE_GREEN;
+    }
+    return(CHOICE_OFF); // If no button is pressed, return none
+}
+
+// Light an LED and play tone
+// Red, upper left:     440Hz - 2.272ms - 1.136ms pulse
+// Green, upper right:  880Hz - 1.136ms - 0.568ms pulse
+// Blue, lower left:    587.33Hz - 1.702ms - 0.851ms pulse
+// Yellow, lower right: 784Hz - 1.276ms - 0.638ms pulse
+void toner(byte which, int buzz_length_ms)
+{
+    setLEDs(which); //Turn on a given LED
+    
+    //Play the sound associated with the given LED
+    switch(which)
+    {
+    case CHOICE_RED:
+        ms.playTone(1136, buzz_length_ms);
+        break;
+    case CHOICE_GREEN:
+        ms.playTone(568, buzz_length_ms);
+        break;
+    case CHOICE_BLUE:
+        ms.playTone(851, buzz_length_ms);
+        break;
+    case CHOICE_YELLOW:
+        ms.playTone(638, buzz_length_ms);
+        break;
+    }
+    
+    setLEDs(CHOICE_OFF); // Turn off all LEDs
+}
+
+// Play the winner sound and lights
+void play_winner(void)
+{
+    setLEDs(CHOICE_GREEN | CHOICE_BLUE);
+    ms.winner_sound();
+    setLEDs(CHOICE_RED | CHOICE_YELLOW);
+    ms.winner_sound();
+    setLEDs(CHOICE_GREEN | CHOICE_BLUE);
+    ms.winner_sound();
+    setLEDs(CHOICE_RED | CHOICE_YELLOW);
+    ms.winner_sound();
+}
+
+
+// Play the loser sound/lights
+void play_loser(void)
+{
+    setLEDs(CHOICE_RED | CHOICE_GREEN);
+    ms.playTone(1500, 255);
+    
+    setLEDs(CHOICE_BLUE | CHOICE_YELLOW);
+    ms.playTone(1500, 255);
+    
+    setLEDs(CHOICE_RED | CHOICE_GREEN);
+    ms.playTone(1500, 255);
+    
+    setLEDs(CHOICE_BLUE | CHOICE_YELLOW);
+    ms.playTone(1500, 255);
+}
+
+// Show an "attract mode" display while waiting for user to press button.
+void attractMode(void)
+{
+    while(1)
+    {
+        setLEDs(CHOICE_RED);
+        delay(100);
+        if (checkButton() != CHOICE_NONE) return;
+        
+        setLEDs(CHOICE_BLUE);
+        delay(100);
+        if (checkButton() != CHOICE_NONE) return;
+        
+        setLEDs(CHOICE_GREEN);
+        delay(100);
+        if (checkButton() != CHOICE_NONE) return;
+        
+        setLEDs(CHOICE_YELLOW);
+        delay(100);
+        if (checkButton() != CHOICE_NONE) return;
     }
 }
--- src/test.h
+++ src/test.h
@@ -9,9 +9,30 @@
 #ifdef __cplusplus
 extern "C" {    //define avr-gcc compiled headers & sources here
 #endif
+enum{
+    CHOICE_OFF=0,
+    CHOICE_RED=1,
+    CHOICE_GREEN=2,
+    CHOICE_BLUE=4,
+    CHOICE_YELLOW=8
+}CHOICE;
+byte CHOICE_NONE=0;
 
 void loop();
 void setup();
+void attractMode(void);
+void setLEDs(byte leds);
+boolean play_memory(void);
+void playMoves(void);
+byte wait_for_button(void);
+void toner(byte which, int buzz_length_ms);
+byte checkButton(void);
+void play_winner(void);
+void play_loser(void);
+
+
+
+
 
 #ifdef __cplusplus
 } // extern "C"
